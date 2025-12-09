afa79f0139b4270fd5fdc6e3a0c0144d027a69d9
Syed Muqeet Aqib <smuqeetaqib@gmail.com>
2025-12-09 11:05:03 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="main-content">{/* Your content goes here */}</main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import './Navbar.css'
+import drivelahLogo from '/drivelah-logo.png'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <img src={drivelahLogo} alt="DriveLab" className="navbar-logo" />
+        </div>
+        <div className="navbar-right">
+          <ul className="navbar-menu">
+            <li><a href="#learn-more">Learn more</a></li>
+            <li><a href="#list-your-car">List your car</a></li>
+            <li><a href="#inbox">Inbox</a></li>
+          </ul>
+          <div className="navbar-profile">
+            <div className="profile-avatar">
+              <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="20" cy="20" r="20" fill="#E5E5E5"/>
+                <circle cx="20" cy="16" r="6" fill="#999999"/>
+                <path d="M8 32C8 26 13 22 20 22C27 22 32 26 32 32V34H8V32Z" fill="#999999"/>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
